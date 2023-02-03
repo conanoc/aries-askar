0eda7cbcbb25f2fa520f3f111168ff97f79908d9
conanoc <conanoc@gmail.com>
2023-02-03 19:40:56 +0900

    Add StoreTests: session.update does not work

--- wrappers/swift/Askar/Sources/Askar/AskarError.swift
+++ wrappers/swift/Askar/Sources/Askar/AskarError.swift
@@ -1,5 +1,18 @@
 import Foundation
 
+public enum AskarErrorCode: Int32 {
+    case Success = 0
+    case Backend = 1
+    case Busy = 2
+    case Duplicate = 3
+    case Encryption = 4
+    case Input = 5
+    case NotFound = 6
+    case Unexpected = 7
+    case Unsupported = 8
+    case Custom = 100
+}
+
 public enum AskarError: LocalizedError {
     case nativeError(code: UInt32)
     case wrapperError(message: String)
---
+++ wrappers/swift/Askar/Tests/AskarTests/StoreTests.swift
@@ -0,0 +1,42 @@
+import XCTest
+@testable import Askar
+
+final class StoreTests: XCTestCase {
+    var store: Store!
+    let TEST_ENTRY = [
+        "category": "test category",
+        "name": "test name",
+        "value": "test_value",
+        "tags": "{\"~plaintag\": \"a\", \"enctag\": {\"b\", \"c\"}}"
+    ]
+
+    override func setUp() async throws {
+        try await super.setUp()
+
+        let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+        let storeURL = temporaryDirectoryURL.appendingPathComponent("test.db")
+        let key = try Store.generateRawKey()
+        store = try await Store.provision(path: storeURL.path, keyMethod: "raw", passKey: key, recreate: true)
+        try await store.doOpenSession()
+    }
+
+    override func tearDown() async throws {
+        try await Store.remove(path: store.path)
+        try await super.tearDown()
+    }
+
+    func testInsertUpdate() async throws {
+        print("rust_log: ", ProcessInfo.processInfo.environment["RUST_LOG"])
+        let session = store.openSession!
+        try await session.update(
+            operation: EntryOperation.INSERT,
+            category: TEST_ENTRY["category"]!,
+            name: TEST_ENTRY["name"]!,
+            value: TEST_ENTRY["value"]!,
+            tags: TEST_ENTRY["tags"]!)
+            
+        let count = try await session.count(category: TEST_ENTRY["category"]!,
+                                            tagFilter: "{\"~plaintag\": \"a\", \"enctag\": \"b\"}")
+        XCTAssertEqual(count, 1)
+    }
+}
