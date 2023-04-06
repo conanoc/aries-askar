3adf0a5c51fa263460d364b59a3ac27136bcaeea
conanoc <conanoc@gmail.com>
2023-04-06 17:03:11 +0900

    Add Dummy project for test

---
+++ wrappers/swift-test/Dummy/Dummy/DummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyApp.swift
+//  Dummy
+//
+//  Created by conanoc on 2023/03/30.
+//
+
+import SwiftUI
+
+@main
+struct DummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
