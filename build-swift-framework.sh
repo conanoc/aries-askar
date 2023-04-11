a975cdc368066ba1a92d76bc49b351d3b4236332
conanoc <conanoc@gmail.com>
2023-04-11 20:52:18 +0900

    Fully implement swift build script

--- build-swift-framework.sh
+++ build-swift-framework.sh
@@ -2,26 +2,117 @@
 
 set -eo pipefail
 
-cargo build
-mkdir -p out/include
+NAME="aries_askar"
+VERSION=$(cargo generate-lockfile && cargo pkgid | sed -e "s/^.*#//")
+BUNDLE_IDENTIFIER="org.hyperledger.$NAME"
+LIBRARY_NAME="lib$NAME.dylib"
+FRAMEWORK_LIBRARY_NAME=${NAME}FFI
+FRAMEWORK_NAME="$FRAMEWORK_LIBRARY_NAME.framework"
+XC_FRAMEWORK_NAME="$FRAMEWORK_LIBRARY_NAME.xcframework"
+HEADER_NAME="${NAME}FFI.h"
+MODULEMAP_NAME="${NAME}FFI.modulemap"
+OUT_PATH="out"
+WRAPPER_PATH="wrappers/swift/Askar/Sources/Askar"
 
-cargo run --bin uniffi-bindgen generate uniffi/askar.udl --language swift -o out --lib-file target/debug/libaries_askar.dylib
+AARCH64_APPLE_IOS_PATH="./target/aarch64-apple-ios/release"
+AARCH64_APPLE_IOS_SIM_PATH="./target/aarch64-apple-ios-sim/release"
+X86_64_APPLE_IOS_PATH="./target/x86_64-apple-ios/release"
+AARCH64_APPLE_DARWIN_PATH="./target/aarch64-apple-darwin/release"
+X86_64_APPLE_DARWIN_PATH="./target/x86_64-apple-darwin/release"
 
-cp out/aries_askarFFI.h out/include/
-cp out/aries_askarFFI.modulemap out/include/module.modulemap
-rm -rf out/aries_askarFFI.xcframework
+targets=("aarch64-apple-ios" "aarch64-apple-ios-sim" "x86_64-apple-ios" "aarch64-apple-darwin" "x86_64-apple-darwin")
+
+# Build for all targets
+for target in "${targets[@]}"; do
+  echo "Building for $target..."
+  rustup target add $target
+  cargo build --release --no-default-features --features uffi --target $target
+done
+
+# Generate swift wrapper
+echo "Generating swift wrapper..."
+mkdir -p $OUT_PATH
+cargo build --release --no-default-features --features uffi
+cargo run --features uffi --bin uniffi-bindgen generate uniffi/askar.udl --language swift -o $OUT_PATH --lib-file ./target/release/$LIBRARY_NAME
+
+# Merge libraries with lipo
+echo "Merging libraries with lipo..."
+lipo -create $AARCH64_APPLE_IOS_SIM_PATH/$LIBRARY_NAME \
+             $X86_64_APPLE_IOS_PATH/$LIBRARY_NAME \
+     -output $OUT_PATH/sim-$LIBRARY_NAME
+lipo -create $AARCH64_APPLE_DARWIN_PATH/$LIBRARY_NAME \
+             $X86_64_APPLE_DARWIN_PATH/$LIBRARY_NAME \
+     -output $OUT_PATH/macos-$LIBRARY_NAME
+
+# Create framework template
+rm -rf $OUT_PATH/$FRAMEWORK_NAME
+mkdir -p $OUT_PATH/$FRAMEWORK_NAME/Headers
+mkdir -p $OUT_PATH/$FRAMEWORK_NAME/Modules
+cp $OUT_PATH/$HEADER_NAME $OUT_PATH/$FRAMEWORK_NAME/Headers
+cp $OUT_PATH/$MODULEMAP_NAME $OUT_PATH/$FRAMEWORK_NAME/Modules/module.modulemap
+
+cat <<EOT > $OUT_PATH/$FRAMEWORK_NAME/Info.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleDevelopmentRegion</key>
+	<string>en</string>
+	<key>CFBundleExecutable</key>
+	<string>$FRAMEWORK_LIBRARY_NAME</string>
+	<key>CFBundleIdentifier</key>
+	<string>$BUNDLE_IDENTIFIER</string>
+	<key>CFBundleInfoDictionaryVersion</key>
+	<string>6.0</string>
+	<key>CFBundleName</key>
+	<string>$FRAMEWORK_LIBRARY_NAME</string>
+	<key>CFBundlePackageType</key>
+	<string>FMWK</string>
+	<key>CFBundleShortVersionString</key>
+	<string>1.0</string>
+	<key>CFBundleVersion</key>
+	<string>$VERSION</string>
+	<key>NSPrincipalClass</key>
+	<string></string>
+</dict>
+</plist>
+EOT
+
+# Prepare frameworks for each platform
+rm -rf $OUT_PATH/frameworks
+mkdir -p $OUT_PATH/frameworks/sim
+mkdir -p $OUT_PATH/frameworks/ios
+mkdir -p $OUT_PATH/frameworks/macos
+cp -r $OUT_PATH/$FRAMEWORK_NAME $OUT_PATH/frameworks/sim/
+cp -r $OUT_PATH/$FRAMEWORK_NAME $OUT_PATH/frameworks/ios/
+cp -r $OUT_PATH/$FRAMEWORK_NAME $OUT_PATH/frameworks/macos/
+mv $OUT_PATH/sim-$LIBRARY_NAME $OUT_PATH/frameworks/sim/$FRAMEWORK_NAME/$FRAMEWORK_LIBRARY_NAME
+mv $OUT_PATH/macos-$LIBRARY_NAME $OUT_PATH/frameworks/macos/$FRAMEWORK_NAME/$FRAMEWORK_LIBRARY_NAME
+cp $AARCH64_APPLE_IOS_PATH/$LIBRARY_NAME $OUT_PATH/frameworks/ios/$FRAMEWORK_NAME/$FRAMEWORK_LIBRARY_NAME
+
+# Create xcframework
+echo "Creating xcframework..."
+rm -rf $OUT_PATH/$XC_FRAMEWORK_NAME
 xcodebuild -create-xcframework \
-    -library target/debug/libaries_askar.a \
-    -headers out/include \
-    -output out/aries_askarFFI.xcframework
+    -framework $OUT_PATH/frameworks/sim/$FRAMEWORK_NAME \
+    -framework $OUT_PATH/frameworks/ios/$FRAMEWORK_NAME \
+    -framework $OUT_PATH/frameworks/macos/$FRAMEWORK_NAME \
+    -output $OUT_PATH/$XC_FRAMEWORK_NAME
 
-# Need some workarounds to compile swift wrapper
+# Fix identifiers of the library
+echo "Fixing identifiers of the library..."
+install_name_tool -id  @rpath/$FRAMEWORK_NAME/$FRAMEWORK_LIBRARY_NAME $OUT_PATH/$XC_FRAMEWORK_NAME/ios-arm64/$FRAMEWORK_NAME/$FRAMEWORK_LIBRARY_NAME
+install_name_tool -id  @rpath/$FRAMEWORK_NAME/$FRAMEWORK_LIBRARY_NAME $OUT_PATH/$XC_FRAMEWORK_NAME/ios-arm64_x86_64-simulator/$FRAMEWORK_NAME/$FRAMEWORK_LIBRARY_NAME
+install_name_tool -id  @rpath/$FRAMEWORK_NAME/$FRAMEWORK_LIBRARY_NAME $OUT_PATH/$XC_FRAMEWORK_NAME/macos-arm64_x86_64/$FRAMEWORK_NAME/$FRAMEWORK_LIBRARY_NAME
+
+# Copy swift wrapper
+# Need some temporary workarounds to compile swift wrapper
 # https://github.com/mozilla/uniffi-rs/issues/1505
 # https://github.com/rust-lang/cargo/issues/11953
-sed -i '' 's/ void \* _Nonnull/UnsafeMutableRawPointer/g' out/aries_askar.swift
-cat > /tmp/import.txt <<EOF
+sed -i '' 's/ void \* _Nonnull/UnsafeMutableRawPointer/g' $OUT_PATH/$NAME.swift
+cat <<EOT > $OUT_PATH/import.txt
 #if os(macOS)
 import SystemConfiguration
 #endif
-EOF
-cat /tmp/import.txt out/aries_askar.swift > wrappers/swift/Askar/Sources/Askar/aries_askar.swift
+EOT
+cat $OUT_PATH/import.txt $OUT_PATH/$NAME.swift > $WRAPPER_PATH/$NAME.swift
