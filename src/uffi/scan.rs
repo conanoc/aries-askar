ff67871a84a7e929621b9b1efdad86966164e331
conanoc <conanoc@gmail.com>
2023-06-19 14:54:03 +0900

    Use Mutex instead of RwLock to make AskarScan Sync

--- src/uffi/scan.rs
+++ src/uffi/scan.rs
@@ -1,27 +1,24 @@
 use std::sync::Arc;
-use tokio::sync::RwLock;
+use tokio::sync::Mutex;
 use crate::{
     uffi::{error::ErrorCode, entry::AskarEntry},
     storage::{Entry, Scan},
 };
 
 pub struct AskarScan {
-    scan: RwLock<Scan<'static, Entry>>,
+    scan: Mutex<Scan<'static, Entry>>,
 }
-
-// Not sure if this is safe, but uniffi requires Sync.
-unsafe impl<'s, T> Sync for Scan<'s, T> {}
 
 impl AskarScan {
     pub fn new(scan: Scan<'static, Entry>) -> Self {
-        Self { scan: RwLock::new(scan) }
+        Self { scan: Mutex::new(scan) }
     }
 }
 
 #[uniffi::export(async_runtime = "tokio")]
 impl AskarScan {
     pub async fn next(&self) -> Result<Option<Vec<Arc<AskarEntry>>>, ErrorCode> {
-        let mut scan = self.scan.write().await;
+        let mut scan = self.scan.lock().await;
         let entries = scan.fetch_next().await?;
         let entries: Vec<Arc<AskarEntry>> = entries
             .unwrap_or(vec![])
@@ -36,7 +33,7 @@ impl AskarScan {
     }
 
     pub async fn fetch_all(&self) -> Result<Vec<Arc<AskarEntry>>, ErrorCode> {
-        let mut scan = self.scan.write().await;
+        let mut scan = self.scan.lock().await;
         let mut entries = vec![];
         while let Some(mut batch) = scan.fetch_next().await? {
             entries.append(&mut batch);
